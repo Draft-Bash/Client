2b72340461df0ce43813a0c1253dd10d21258ed9
Stephen Feddes <stephenpfeddes@lewisu.edu>
2023-09-14 18:39:08 -0500

    testing ci/cd

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
+// Renders the App.
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
