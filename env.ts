9c4da5c3f4c8cbc6de9d900819ea9c71ccc500c6
StephenFeddes <112678486+StephenFeddes@users.noreply.github.com>
2023-09-15 22:59:02 -0500

    Added the environment variables for the test environment.

--- env.ts
+++ env.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://draftbash.azurewebsites.net/api/v1"
-export const SERVER_URL = "https://draftbash.azurewebsites.net"
+export const API_URL = "https://draftbash-test.azurewebsites.net/api/v1"
+export const SERVER_URL = "https://draftbash-test.azurewebsites.net"
