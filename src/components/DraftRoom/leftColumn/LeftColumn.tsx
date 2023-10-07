59246b925f18dd7df459259b1610f6a7e071ef77
Brian Feddes <brianfeddes@gmail.com>
2023-10-06 21:28:54 -0500

    Revert "Refactored code."
    This reverts commit 76c163936bbf8c5dfba0277d32f441b817dcfb61.

--- src/components/DraftRoom/leftColumn/LeftColumn.tsx
+++ src/components/DraftRoom/leftColumn/LeftColumn.tsx
@@ -5,6 +5,7 @@ import UserPickQueue from './UserPickQueue';
 import DraftRoster from './DraftRoster';
 import { useDraft } from '../DraftContext';
 import { useAuth } from '../../../authentication/AuthContext';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const LeftColumn = () => {
 
@@ -16,6 +17,19 @@ const LeftColumn = () => {
 
   useEffect(() => {
     if (userId && draftId) {
+      fetch(API_URL+`/drafts/autodraft?userId=${userId}&draftId=${draftId}`)
+      .then(response => {
+        return response.json(); // Parse the response as JSON
+      })
+      .then(data => {
+        console.log(data)
+        setAutopickStatus(data);
+      })
+      .catch(error => {
+        // Handle any errors that occurred during the fetch
+        console.error('Fetch error:', error);
+      });
+
       socket?.on('autodraft-enabled', (autodrafterId) => {
         if (autodrafterId==userId) {
           setAutopickStatus(true);
@@ -26,7 +40,27 @@ const LeftColumn = () => {
 
   async function toggleAutodraft() {
     socket?.emit('update-autodraft', !isAutopickOn);
-    setAutopickStatus(!isAutopickOn);
+    fetch(API_URL+`/drafts/autodraft`, {
+      method: 'POST', // HTTP request method
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        isAutopickOn: !isAutopickOn,
+        userId: userId,
+        draftId: draftId
+      })
+    })
+      .then(response => {
+        return response.json(); // Parse the response as JSON
+      })
+      .then(data => {
+        console.log(!isAutopickOn);
+        setAutopickStatus(!isAutopickOn);
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+      });
   }
 
   return (
@@ -34,7 +68,7 @@ const LeftColumn = () => {
         <header>
           <h4>Pick Queue</h4>
           <ToggleButton 
-            labelName="Autodraft"
+            labelName="Autopick"
             handleOnClick={toggleAutodraft}
             defaultToggleState={isAutopickOn}
           />
